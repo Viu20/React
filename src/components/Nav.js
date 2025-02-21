f6e12ccbcf546b616c71131156efabd96723ce61
Елизавета Колмакова <lizakolmakova2004icloud.com@mail.ru>
2025-02-21 23:38:45 +0700

    Initial commit

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+// src/components/Nav.js
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <nav style={{ padding: '10px', backgroundColor: '#f0f0f0' }}>
+            <a href="/">Home</a> | <a href="/about">About</a>
+        </nav>
+    );
+};
+
+export default Nav;
